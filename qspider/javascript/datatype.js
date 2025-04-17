6932a5075a36cc0ff58980237d70c68b3225f421
vikash sharma <vk220783@gmail.com>
2025-04-17 17:05:00 +0530

    learing javascript

---
+++ qspider/javascript/datatype.js
@@ -0,0 +1,29 @@
+var n1 =10;
+console.log(n1)
+//  this is number type datatype
+console.log(typeof n1)
+
+var n2 ='hello world'
+// this is string data type
+console.log( typeof n2)
+
+var n3 = true;
+ console.log(typeof n3)
+//  this is boolean type of datatype
+
+var n4;
+// the output is undefined
+
+
+console.log(n4)
+
+console.log(typeof typeof n4)
+
+
+// console.log(abc)
+// this is will show not defined 
+
+var n5 = null
+// by developer added the value null for future to add the new value
+
+console.log(typeof n5)
